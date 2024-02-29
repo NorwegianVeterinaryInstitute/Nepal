1f502df2c50084889eb7f69790ae1d0324c41d63
Thomieh73 <Thhaverk@gmail.com>
2024-02-29 10:29:17 +0100

    made sure the nepal.sh script copies all workflow files to the output directory of a run

--- nepal.sh
+++ nepal.sh
@@ -9,7 +9,7 @@ workdir=${3:-$USERWORK/nepal}
 DATE=($(date "+%Y%m%d_%R"))
 mkdir -p ${outdir}/config_files
 mkdir -p ${outdir}/nextflow_reports
-cp ${script_directory}/main.nf ${outdir}/config_files
+cp ${script_directory}/*.nf ${outdir}/config_files
 cp ${config} ${outdir}/config_files
 
 #nextflow_21.10.6 run ${script_directory}/main.nf -c ${config} --out_dir=${outdir} -work-dir ${workdir} -resume -with-report $DATE.report.html
